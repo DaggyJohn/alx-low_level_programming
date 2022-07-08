6d2d06f7a94ab45860e0f4e7450e1e8cded3913c
DaggyJohn <DaggyJohn>
2022-07-07 17:06:32 -0700

    normal tasks of more on functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - multiplier
+ * @a: int to mult
+ * @b: int to mult
+ * Return: result of two
+ */
+int mult(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Tasks on more on functions and nested loops
